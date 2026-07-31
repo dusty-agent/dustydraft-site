1c6dd2586e7945715fc8d0efa1adf449bbad0487
dustydraft <soyoung@dustydraft.com>
2026-07-31 23:53:06 +0900

    chore: update site tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 import Header from "../components/Header";
@@ -34,15 +35,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-h-screen bg-[#ede4da] text-neutral-900 flex flex-col">
-
         <Header />
 
-        <main className="flex-1">
-          {children}
-        </main>
+        <main className="flex-1">{children}</main>
 
         <Footer />
 
+        <Script
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          strategy="afterInteractive"
+          data-cf-beacon='{"token":"c75e6f29ecb44b61b280886115c10d45"}'
+        />
       </body>
     </html>
   );
